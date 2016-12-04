c566160b4ad0efe9f73da3115d72395b9a087ad2
Cody Goodman <codygman.consulting@gmail.com>
2016-12-04 01:34:54 -0600

    add type error

--- src/TH.hs
+++ src/TH.hs
@@ -9,3 +9,29 @@ import Language.Haskell.TH.Syntax
 -- | Creates `Type A = String` Type Synonym at compile time unless it already exists
 createTypSynA :: DecsQ
 createTypSynA = return $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
+
+-- current type error:
+{-
+src/TH.hs:11:17: warning: [-Wdeferred-type-errors] …
+    • Couldn't match type ‘Q Dec’ with ‘Dec’
+      Expected type: DecsQ
+        Actual type: Q [DecQ]
+    • In the expression:
+        return
+        $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
+      In an equation for ‘createTypSynA’:
+          createTypSynA
+            = return
+              $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
+src/TH.hs:11:17: warning: [-Wdeferred-type-errors] …
+    • Couldn't match type ‘Q Dec’ with ‘Dec’
+      Expected type: DecsQ
+        Actual type: Q [DecQ]
+    • In the expression:
+        return
+        $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
+      In an equation for ‘createTypSynA’:
+          createTypSynA
+            = return
+              $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
+-}
