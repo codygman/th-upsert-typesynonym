1458fb5f88d62b99d955ece4cceb4807f6350249
Cody Goodman <codygman.consulting@gmail.com>
2016-12-04 01:57:30 -0600

    working type simple synonym creation

--- src/TH.hs
+++ src/TH.hs
@@ -1,37 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 module TH where
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 
--- | Creates `Type A = String` Type Synonym at compile time unless it already exists
 createTypSynA :: DecsQ
-createTypSynA = return $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
-
--- current type error:
-{-
-src/TH.hs:11:17: warning: [-Wdeferred-type-errors] …
-    • Couldn't match type ‘Q Dec’ with ‘Dec’
-      Expected type: DecsQ
-        Actual type: Q [DecQ]
-    • In the expression:
-        return
-        $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
-      In an equation for ‘createTypSynA’:
-          createTypSynA
-            = return
-              $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
-src/TH.hs:11:17: warning: [-Wdeferred-type-errors] …
-    • Couldn't match type ‘Q Dec’ with ‘Dec’
-      Expected type: DecsQ
-        Actual type: Q [DecQ]
-    • In the expression:
-        return
-        $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
-      In an equation for ‘createTypSynA’:
-          createTypSynA
-            = return
-              $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
--}
+createTypSynA = [d| type A = $([t| String |]) |]
