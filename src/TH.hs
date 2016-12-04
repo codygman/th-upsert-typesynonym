3b78cdb7f238a42b73a50b68f8f1d5797fcfa22f
Cody Goodman <codygman.consulting@gmail.com>
2016-12-04 01:33:22 -0600

    update

--- src/TH.hs
+++ src/TH.hs
@@ -6,5 +6,6 @@ module TH where
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 
+-- | Creates `Type A = String` Type Synonym at compile time unless it already exists
 createTypSynA :: DecsQ
 createTypSynA = return $ (tySynD "A" [] (pure $ LitT (StrTyLit ("a" :: String)))) : []
